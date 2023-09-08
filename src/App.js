82cdf889e8cc820197fbbaddfa99dba2eb680db1
CRUX56 <designers_crux@live.com>
2023-09-08 10:12:59 -0400

    Fixed issue with app routing. App showing properly.

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import {
+  ChannelDetail,
+  VideoDetail,
+  SearchFeed,
   Navbar,
   Feed,
-  VideoDetail,
-  ChannelDetail,
-  SearchFeed,
 } from "./components";
 
-const app = () => {
+const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}>
       <Navbar />
       <Routes>
-        <Route path="/" exact element={<Feed />} />
+        <Route exact path="/" element={<Feed />} />
         <Route path="/video/:id" element={<VideoDetail />} />
         <Route path="/channel/:id" element={<ChannelDetail />} />
         <Route path="/search/:searchTerm" element={<SearchFeed />} />
       </Routes>
     </Box>
-  </BrowserRouter>;
-};
+  </BrowserRouter>
+);
 
-export default app;
+export default App;
